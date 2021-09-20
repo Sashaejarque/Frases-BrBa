d20e7802f0ca300a7476e090ae78191f5b7e6d7b
sashaejarque <sashax191@gmail.com>
2021-09-20 20:16:52 -0300

    App en funcionamiento

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+import Frase from "./components/Frase";
+import logo from "./logo.svg";
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size 0.8s ease;
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
+`;
+
+const Logo = styled.img`
+  height: 230px;
+  width: 230px;
+`;
 
 function App() {
+  // state de frases
+  const [frase, guardarFrase] = useState([
+    
+  ]);
+
+  const consultarAPI = async () => {
+    const api = await fetch(
+      "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
+    const frase = await api.json();
+    guardarFrase(frase[0]);
+  };
+
+  // Cargar una frase
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <Logo src={logo} />
+      <Frase frase={frase} />
+      <Boton onClick={consultarAPI}>Obtener Frase</Boton>
+    </Contenedor>
   );
 }
 
---
+++ src/components/Frase.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+
+const ContenedorFrase = styled.div`
+    padding: 3rem;
+    border-radius: .5rem;
+    background-color: #fff;
+    max-width:800px;
+
+    h1 {
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+        position: relative;
+        padding-left: 4rem;
+
+        &::before {
+            content: open-quote;
+            font-size: 10rem;
+            color: black;
+            position: absolute;
+            left: -1rem;
+            top: -2rem;
+        }
+    }
+    p {
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-size: 1.4rem;
+        font-weight:bold;
+        text-align: right;
+        color: #666;
+        margin-top: 2rem;
+    }
+`;
+
+const Frase = ({frase}) => {
+
+    return ( 
+        <ContenedorFrase>
+            <h1>{frase.quote} </h1>
+            <p>- {frase.author} </p>
+        </ContenedorFrase>
+     );
+}
+ 
+export default Frase;
